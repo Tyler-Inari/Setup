be51bd3a6e2618d8c10d17cb52c69a344a7793da
Tyler-Inari <s1922054@stu.musashino-u.ac.jp>
2022-07-31 15:05:28 -0700

    init sh

---
+++ sh/github.sh
@@ -0,0 +1,17 @@
+#!/usr/local/bin/zsh
+CONFIG=".git/config"
+if [ -e  $CONFIG ]; then
+  FILE=$(<$CONFIG)
+  GITHUB_HTTPS="https://github.com/*"
+  GITHUB_GITAT="git@github.com:*"
+  for line in $FILE
+  do
+    if [[ $line = $GITHUB_HTTPS ]]; then
+      open $line
+    elif [[ $line = $GITHUB_GITAT ]]; then
+      open ${line/git@github.com:/https://github.com/}
+    fi
+  done
+else
+  open "https://github.com/"
+fi
